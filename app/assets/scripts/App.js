9abbe65dcfbb71f81d9c59dbecca1fff42d76431
paul <paulrooney60@gmail.com>
2020-04-11 20:37:21 +0100

    completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,10 +7,40 @@ let mobileMenu= new MobileMenu();
 
 let stickyheader= new StickyHeader();
 
+let modal;
+
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
+
 if (module.hot) {
   module.hot.accept()
 }
+
+document.querySelectorAll(".open-modal").forEach(el => {
+	   el.addEventListener("click", e => {
+		   
+		   e.preventDefault()
+		   if (typeof modal == "undefined"){
+			   
+			   import(/* webpackChunkName: "modal"*/"./modules/modal").then(x => {
+			  modal = new x.default()
+			  setTimeout(() => modal.openTheModal(), 20)
+			   
+		   }).catch(() => console.log("there was a problem"))
+			   
+			   
+		   }else{
+			   
+			   modal.openTheModal()
+			   
+		   }
+
+
+           
+		   
+	   })
+	
+})
+
