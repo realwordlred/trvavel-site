05578d59e98e76a91b49b7d080865f8b894ddfc5
paul <paulrooney60@gmail.com>
2020-04-15 12:46:11 +0100

    edited client area

--- app/assets/scripts/modules/clientarea.js
+++ app/assets/scripts/modules/clientarea.js
@@ -30,7 +30,7 @@ class ClientArea {
 	}
 	
 	sendRequest() {
-		axios.post().then(response => {
+		axios.post("https://gracious-boyd-7d3a60.netlify.com/.netlify/functions/secret-area", { password: this.field.value}).then(response => {
 			this.form.remove()
 			this.contentArea.innerHTML = response.data
 		
