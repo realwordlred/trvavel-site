1e6d384293fba01ede69efb764bb25d0a7a710d8
paul <paulrooney60@gmail.com>
2020-04-15 10:00:15 +0100

    client area password setup

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,19 @@
 exports.handler = function(event, context, callback){
+	
+	let body
+	
+	if(event.body){
+		
+		body = JSON.parse(event.body)
+		
+		
+	}else{
+		body = {}
+		
+	}
+	
+	
+	
 	callback( null, {
 		statusCode: 200,
 		body:"welcome to the super secret area"
